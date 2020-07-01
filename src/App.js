252c9834438b191a32446041d0694b312ced13e9
Deepali Abhyankar <deepali.abhyankar@marriott.com>
2020-07-01 12:24:20 -0400

    Initial code commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch} from "react-redux";
+import axios from "axios";
+import Withdrawal from "./components/Withdrawal";
+import RecentTransactions from "./components/RecentTransactions";
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  function getTransactions() {
+    return dispatch => {
+      axios.get("https://app.fakejson.com/q/0Pm3bJKu?token=HbqwPS-BSqOehLpig2ePqg")
+      .then(res =>{
+          dispatch({
+            type: "UPDATE_TRANSACTIONS",
+            payload: res.data.transactions
+          })
+      });
+    };
+  }
+
+  useEffect(() => {
+    dispatch(getTransactions());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-center">
+      <Withdrawal />
+      <RecentTransactions />  
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducer from "./stores/store.js";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  rootElement
 );
 
 // If you want your app to work offline and load faster, you can change
